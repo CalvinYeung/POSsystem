64368f508cede71f6fcab57e4362359591c7f65c
bash (curl -sL https://raw.githubusercontent.com/ga-instructors/installfest_script/pluto/builds/mac) <calyeung92@gmail.com>
2015-08-26 09:38:07 -0400

    first commit

---
+++ notepage.js
@@ -0,0 +1,18 @@
+1 - As a WORKER, I want to CREATE A NEW ORDER so I can enter WHAT THE CUSTOMER WANTS TO ORDER
+
+2 - as a worker, I want to LABEL ORDERS with names so I can KEEP TRACK OF ORDERS
+
+3 - as a WORKER, I want to ADD ITEMS BY CLICKING to the order so I can UPDATE THE ORDER
+
+4 - as a worker, I want to REMOVE ITEMS from the order in case I make a mistake or customer changes mind
+
+5 - as a worker, 
+
+Meiji - "meiji's melt in your mouth marshmallows"
+Josh - "Josh's Jelly Beans"
+Pierre - Pierre Douce chocolate
+Toti - Valrhonalicious
+Jaskar - Jaskar Jujubes
+Nick - Marsh marshmallows
+
+
