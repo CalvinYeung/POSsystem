20850856e41196b5a71a3a48bad6d850a851476b
bash (curl -sL https://raw.githubusercontent.com/ga-instructors/installfest_script/pluto/builds/mac) <calyeung92@gmail.com>
2016-04-14 15:38:42 -0400

    Re-did CSS

--- pos.js
+++ pos.js
@@ -1,47 +1,59 @@
 jQuery(function(){
+
 	var candy = [
 	{name: "Calvin's Crack Candy Cane", price: 13337},
-	{name: "Meiji's Melt In Your Mouth Marsmallows", price: 25 },
+	{name: "Meiji's Marsmallows", price: 25 },
 	{name: "Josh's JellyBeans", price: 55 },
-	{name: "Harrison's Trixies Treats", price: 10.00 },
-	{name: "Brian's Schweddy Balls", price: 99 },
+	{name: "Harrison's Treats", price: 10.00 },
+	{name: "Brian's FireBalls", price: 99 },
 	{name: "Ed's Edibles", price: 5},
 	{name: "Zoe's Zesty Zucchini", price: 8 },
 	{name: "Jacob Juicy", price: 23 },
-	{name: "Raquel's Roasted and Toasted Smores", price: 54 },
+	{name: "Raquel's Smores", price: 54 },
 	{name: "Marshall Marshmallow", price: 75 },
 	{name: "Mohamed KitKat", price: 95},
 	{name: "Pierre Douce Chocolate", price: 21 },
 	{name: "Jaskar Jujubes", price: 31},
 	{name: "Toti's Chocotruffle", price: 75},
-	{name: "Yuka's Peppermint Patties", price: 12 },
-	{name: "Eric's altoid-shaped questionable orange tablet candy", price: 1},
+	{name: "Yuka's Peppermint", price: 12 },
+	{name: "Eric's orange tablet", price: 1},
 	{name: "Pam's Pineapple", price: 45 },
 	{name: "Pan's Pancakes", price: 71},
 	{name: "Dennis's DubbleBubble", price: 25 },
 	{name: "Jaden's Jawbreakers", price: 23 },
-	{name: "Bobby's Frozen Bananas", price: 30 },
-	{name: "Joy's Soy Candy", price: 99 },
-	{name: "Steph's Sour Patch Kids", price: 43 },
-	{name: "Jennifers Blue Sesame Pop", price: 85},
-	{name: "Smither's Chocolate Smothered Cashews", price: 65},
-	];
+	{name: "Bobby's Bananas", price: 30 },
+	{name: "Joy's Soy", price: 99 },
+	{name: "Steph's Sours", price: 43 },
+	{name: "Jennifers Pop", price: 85}];
 
 /////////////////////////////////////////////////
 	// Populate the menu with Pieces of Candy
-	var menu = $("#menu");
+	var menu = $(".menu");
 	var orderArray = [];
 
 	var makeMenu = function(){
 		candy.forEach(function(pieceOfCandy, index){
-	    var menuItemDiv = $("<button>", {id: index, class: "menuItem"});
-	    
+	    var menuItemDiv = $("<button>", {id: index, class: "menuItem col-xs-3"});
 		menuItemDiv.append(pieceOfCandy.name + "\n" + "$"+ pieceOfCandy.price);
 		menu.append(menuItemDiv);
 		})
 	};
-
 	makeMenu();
+
+	var divs = $("div.menu > button");
+	var makeRow = function() {
+		for(var i = 0; i < divs.length; i+=4) {
+  			divs.slice(i, i+4).wrapAll("<div class='row'></div>");
+		}
+	};
+	makeRow();
+
+ // var divs = $("div > h1");
+ //    for(var i = 0; i < divs.length; i+=4) {
+ //      divs.slice(i, i+4).wrapAll("<div class='new'></div>");
+ //    }
+
+
 /////////////////////////////////////////////////
 	var newOrderButton = $("#newOrderButton");
 	var newOrderUL = $("#newOrderUL");
@@ -51,7 +63,7 @@ jQuery(function(){
 		event.preventDefault();
 		var customerName = prompt("What name would you like under your order?");
 		orderArray.push({customerName:customerName, items:[]});
-		var newOrderLI = $("<button>", {class: "newOrderLI", id: orderArray.length -1});
+		var newOrderLI = $("<button>", {class: "newOrderLI col-sm-12", id: orderArray.length -1});
 		orderToBePopulated = orderArray[orderArray.length -1].items
 		//appending
 		newOrderLI.append(customerName);
@@ -108,6 +120,20 @@ jQuery(function(){
 					event.preventDefault();
 				})
 		};
+
+	var currentOrderBox = $(".currentOrderBox");
+	var menuBox = $(".menuBox");
+	var height = menuBox.height();
+	var matchHeight = function(currentOrderBox, menuBox) {
+		currentOrderBox.css("height", height);
+	};
+	matchHeight();
+
 });//END OF jQUERY
+
+// function setHeight(elem1, elem2) {
+//   var height = elem2.height()
+//   elem1.css('height', height); 
+// }
 
 
